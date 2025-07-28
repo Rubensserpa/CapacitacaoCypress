42648f2a7611661e0ec4770ffe20a3bb6315cbf4
Rubens Pereira Serpa (Indra) <Rubens.Serpa@fornecedores.sicoob.com.br>
2025-07-28 20:15:57 -0300

    iniciando o projeto

---
+++ cypress/e2e/Teste.cy.js
@@ -0,0 +1,11 @@
+describe('Teste - Formulário', () => {
+  it('Preencher campos com sucesso', () => {
+    cy.visit('https://testautomationpractice.blogspot.com/')
+    cy.get('#name').type('Teste')
+    cy.get('input[placeholder="Enter EMail"]').type('teste@gmail.com')
+    cy.get('#phone').type('9865684235')
+
+
+  })
+ 
+})
